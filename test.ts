7593eb13c94db242ec1fce842c4d9ed25dbb6342
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-04-05 09:32:20 -0700

    added iage in example

--- test.ts
+++ test.ts
@@ -3,7 +3,27 @@ namespace dom {
 function listComponent(items: string[], selected: number) {
     const el = verticalFlow(
         items.map((name, index) => box(
-            text(name),
+            dom.horizontalFlow([
+                imageElement(img`
+                    . . . . . . . . . . b 5 b . . .
+                    . . . . . . . . . b 5 b . . . .
+                    . . . . . . b b b b b b . . . .
+                    . . . . . b b 5 5 5 5 5 b . . .
+                    . . . . b b 5 d 1 f 5 d 4 c . .
+                    . . . . b 5 5 1 f f d d 4 4 4 b
+                    . . . . b 5 5 d f b 4 4 4 4 b .
+                    . . . b d 5 5 5 5 4 4 4 4 b . .
+                    . . b d d 5 5 5 5 5 5 5 5 b . .
+                    . b d d d d 5 5 5 5 5 5 5 5 b .
+                    b d d d b b b 5 5 5 5 5 5 5 b .
+                    c d d b 5 5 d c 5 5 5 5 5 5 b .
+                    c b b d 5 d c d 5 5 5 5 5 5 b .
+                    . b 5 5 b c d d 5 5 5 5 5 d b .
+                    b b c c c d d d d 5 5 5 b b . .
+                    . . . c c c c c c c c b b . . .
+                `),
+                text(name)
+            ]),
             [className("label-box"), index === selected ? className("label-selected") : null]
         ))
     );
@@ -37,6 +57,24 @@ function testV() {
     const elg = () => verticalFlow([
     box(nameView(), [width(FILL), alignRight(), borderRight(2)]),
         horizontalFlow([
+            imageElement(img`
+                . . . . . . . . . . b 5 b . . .
+                . . . . . . . . . b 5 b . . . .
+                . . . . . . b b b b b b . . . .
+                . . . . . b b 5 5 5 5 5 b . . .
+                . . . . b b 5 d 1 f 5 d 4 c . .
+                . . . . b 5 5 1 f f d d 4 4 4 b
+                . . . . b 5 5 d f b 4 4 4 4 b .
+                . . . b d 5 5 5 5 4 4 4 4 b . .
+                . . b d d 5 5 5 5 5 5 5 5 b . .
+                . b d d d d 5 5 5 5 5 5 5 5 b .
+                b d d d b b b 5 5 5 5 5 5 5 b .
+                c d d b 5 5 d c 5 5 5 5 5 5 b .
+                c b b d 5 d c d 5 5 5 5 5 5 b .
+                . b 5 5 b c d d 5 5 5 5 5 d b .
+                b b c c c d d d d 5 5 5 b b . .
+                . . . c c c c c c c c b b . . .
+            `),
             box(statsView(), [border(2)]),
             box(detailView(), [border(4), borderColor(3)])
         ])
